0679f6b908d72bf7785e0aa8d078d51321711ef8
Sam Lin <greenlin111@gmail.com>
2019-10-12 00:29:33 +0800

    ✨ added httpoison GET/POST

---
+++ lib/braze/http_client.ex
@@ -0,0 +1,29 @@
+defmodule Braze.HttpClient do
+  @moduledoc false
+
+  def post(url, body \\ "", options \\ nil) do
+    case Httpoison.post(url, body, options) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        IO.puts(body)
+
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+        IO.puts("Not found :(")
+
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        IO.inspect(reason)
+    end
+  end
+
+  def get(url) do
+    case HTTPoison.get(url) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        IO.puts(body)
+
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+        IO.puts("Not found :(")
+
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        IO.inspect(reason)
+    end
+  end
+end
