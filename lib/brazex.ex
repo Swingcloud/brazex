d841f6ccef53e42e62e6f5f249d315c462d79c2a
Sam Lin <greenlin111@gmail.com>
2019-10-12 00:10:22 +0800

    🎉 init brazex

---
+++ lib/brazex.ex
@@ -0,0 +1,18 @@
+defmodule Brazex do
+  @moduledoc """
+  Documentation for Brazex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Brazex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/brazex_test.exs
@@ -0,0 +1,8 @@
+defmodule BrazexTest do
+  use ExUnit.Case
+  doctest Brazex
+
+  test "greets the world" do
+    assert Brazex.hello() == :world
+  end
+end
